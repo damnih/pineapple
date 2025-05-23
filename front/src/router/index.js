bad6b0803e242b1e41f771540291668c21ee5ab8
username <email>
2025-05-23 12:11:11 +0900

    MapView not finished

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -6,6 +6,7 @@ import YoutubeView from '@/views/YoutubeView.vue'
 import ArticleView from '@/views/ArticleView.vue'
 import SignupView from '@/views/SignupView.vue'
 import LoginView from '@/views/LoginView.vue'
+import MapView from '@/views/MapView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -44,6 +45,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView,
+    },
+    {
+      path: '/map',
+      name: 'map',
+      component: MapView,
     },
   ],
 })
