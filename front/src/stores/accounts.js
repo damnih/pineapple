6a1ceca3889ee165e4bda3186a51618dcac6f2f2
damdamh <dmmeeham@gmail.com>
2025-05-26 09:16:51 +0900

    still doing login.. how can make it act?

--- front/src/stores/accounts.js
+++ front/src/stores/accounts.js
@@ -7,7 +7,14 @@ export const useAccountStore = defineStore('account', () => {
   const ACCOUNT_API_URL = 'http://127.0.0.1:8000/accounts'
   const token = ref('')
 
-    const signUp = function ({username, password, password2, age, name, nationality}) {
+    
+  // const signUp = ({ username, password, password2, age, name, nationality }) => {
+  //   return axios.post(`${BASE}/signup/`, {
+  //     username, password, password2, age, name, nationality
+  //   })
+  // }
+  
+  const signUp = function ({username, password, password2, age, name, nationality}) {
     axios({
       method: 'POST',
       url: `${ACCOUNT_API_URL}/signup/`,
@@ -22,11 +29,16 @@ export const useAccountStore = defineStore('account', () => {
     })
       .then(res => {
         console.log('회원가입 성공!')
+        // 회원가입 성공 시 팝업과 함께 메인 페이지로 이동하는 로직 추가 
+        alert(res.data.message)  // "회원가입 성공"
+        router.push('/') 
       })
       .catch(err => console.log(err))
   }
 
-    const logIn = function({id, pw}) {
+
+  
+  const logIn = function({id, pw}) {
     axios({
       method: 'POST',
       url: `${ACCOUNT_API_URL}/login/`,
@@ -37,7 +49,10 @@ export const useAccountStore = defineStore('account', () => {
       .then(res => {
         token.value = res.data.key
       })
-      .catch(err => console.log(err))
+      .catch(err => {
+        // console.log(err.response.data)          // ← 이 한 줄로, 서버가 보낸 에러 메시지를 찍을 수 있습니다.
+        alert(err.response.data.errors)         // ← 사용자에게도 보여주고 싶다면
+      })
   }
 
 
--- front/src/views/LoginView.vue
+++ front/src/views/LoginView.vue
@@ -3,12 +3,12 @@
       <form @submit.prevent="onLogIn">
         <div class="mb-3">
           <label for="id" class="form-label fw-semibold">ID</label>
-          <input type="text" class="form-control" id="id" placeholder="Enter your ID" />
+          <input v-model="id" type="text" class="form-control" id="id" placeholder="Enter your ID" />
         </div>
 
         <div class="mb-3">
           <label for="password" class="form-label fw-semibold">Password</label>
-          <input type="password" class="form-control" id="pw" placeholder="Enter your password" />
+          <input v-model="pw" type="password" class="form-control" id="pw" placeholder="Enter your password" />
         </div>
 
         <button type="submit" class="btn btn-primary w-100">Log In</button>
@@ -31,6 +31,7 @@
       id: id.value,
       pw: pw.value
     }
+    console.log(id.value, pw.value)
     accountStore.logIn(userInfo)
   }
 </script>
